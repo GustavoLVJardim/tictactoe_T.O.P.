2ba59633f45732b85f547bae1f899ba2b8603a4e
gustavoLVJardim <utilizador@Mac-mini-de-utilizador.local>
2025-02-09 11:15:11 +0000

    atualizations

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,8 +1,9 @@
 class Game
   def name_players
     @names = take_name_player()
-    puts "The name is saved here: #{@names}"
-    
+    gets.chomp
+    #puts "The name is saved here: #{@names}"
+    return @names
   end
 
  def begin_screen
@@ -31,15 +32,16 @@ class Game
  end
 
  def take_name_player
-   puts "What's your name, player 1?"
-   name_player1 = gets.chomp
-   puts "Hello, #{name_player1}!"
-
-   puts "What's your name, player 2?"
-   name_player2 = gets.chomp
-   puts "Hello, #{name_player2}!"
-
-   return name_player1, name_player2
+  puts "What's your name, player 1?"
+  name_player1 = gets.chomp
+  puts "Hello, #{name_player1}!"
+  puts 'you will be the "x"'
+  puts ""
+  puts "What's your name, player 2?"
+  name_player2 = gets.chomp
+  puts "Hello, #{name_player2}!"
+  puts 'you will be the "0"' 
+  return name_player1, name_player2
  end
 
  def puts_the_rules
@@ -63,6 +65,14 @@ def clear_screen
   system("clear") || system("cls")
 end
 
+def game_play()
+  puts ""
+  puts "Player 1: #{@names[0]} vs Player 2: #{@names[1]}"
+  puts ""
+  puts "Score => Player 1: #{0} | Player 2: #{0}"
+  game_board
+end
+
 
 
 
@@ -71,9 +81,10 @@ end
 game1 = Game.new()
 game1.begin_screen
 game1.game_board
+#game1.clear_screen
 game1.puts_the_rules
-#game1.take_name_player
+game1.clear_screen
 game1.name_players
 game1.clear_screen
-
+game1.game_play()
 
